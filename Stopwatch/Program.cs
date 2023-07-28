092ab25ea7857e8dbc432badf94184d7a846e34d
Guilherme Borret Oliveira Ferreira <gborret@icloud.com>
2023-07-28 15:56:12 -0300

    Primeiro commit

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Timers;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static bool isPaused = false;
+        static System.Timers.Timer timer = new System.Timers.Timer(1000); // Atualizar o cronômetro a cada 1 segundo
+        static int remainingTime = 0;
+        static int totalTime = 0; // Tempo total da contagem regressiva
+        static object lockObject = new object(); // Variável de bloqueio para evitar execução concorrente
+
+        // Função principal que inicia o programa
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindo ao Stopwatch!");
+            System.Threading.Thread.Sleep(2000); // Aguarda 2 segundos antes de exibir o menu
+            Menu();
+        }
+
+        // Função para exibir o menu principal
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = (Console.ReadLine() ?? "").ToLower();
+
+            // Verificar se a entrada é '0' para sair do programa
+            if (data == "0")
+            {
+                System.Environment.Exit(0);
+            }
+
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+            // Tenta converter a parte numérica da entrada em um número inteiro
+            if (!int.TryParse(data.Substring(0, data.Length - 1), out int time))
+            {
+                Console.WriteLine("Entrada inválida. Tente novamente.");
+                System.Threading.Thread.Sleep(2000);
+                Menu();
+                return;
+            }
+
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            totalTime = time * multiplier;
+            PreStart(totalTime);
+        }
+
+        // Função que prepara o cronômetro para iniciar a contagem regressiva
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Preparar...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Apontar...");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine("Sua contagem regressiva começou!");
+            System.Threading.Thread.Sleep(2500);
+
+            Start(time);
+        }
+
+        // Função que realiza a contagem regressiva
+
+        static void Start(int time)
+        {
+            remainingTime = time;
+
+            // Configurar o timer para atualizar o cronômetro a cada segundo
+            timer.Elapsed += UpdateTimer;
+            timer.Start();
+
+            while (remainingTime > 0)
+            {
+                if (!isPaused)
+                {
+                    // Mostra a contagem regressiva em tempo real
+                    Console.Clear();
+                    Console.WriteLine("Pressione 'P' para pausar");
+                    Console.WriteLine("");
+                    Console.WriteLine($"Tempo restante: {remainingTime} segundos.");
+                }
+
+                // Aguarda 1 segundo antes de atualizar o cronômetro novamente
+                System.Threading.Thread.Sleep(1000);
+
+                // Verifica se o usuário quer pausar o cronômetro
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true).Key;
+                    if (key == ConsoleKey.P)
+                    {
+                        isPaused = !isPaused; // Inverte o estado da pausa
+                        if (isPaused)
+                        {
+                            Console.WriteLine("\nCronômetro pausado. Pressione 'P' para retomar...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nRetomando a contagem regressiva...");
+                        }
+                    }
+                }
+            }
+
+            // Parar o timer e a execução do cronômetro
+            timer.Stop();
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado!");
+            System.Threading.Thread.Sleep(2500);
+            Menu();
+        }
+
+        // Função que atualiza o cronômetro a cada segundo
+
+        static void UpdateTimer(object sender, ElapsedEventArgs e)
+        {
+            lock (lockObject) // Bloquear a execução concorrente do código
+            {
+                if (!isPaused && remainingTime > 0)
+                {
+                    remainingTime--;
+                }
+            }
+        }
+    }
+}
---
+++ TextEditor/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir um arquivo existente");
+            Console.WriteLine("2 - Criar um novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+        }
+        static void Open()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("-----------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Qual o local para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Seu arquivo {path} foi salvo com SUCESSO!");
+            Console.ReadLine();
+            Menu();
+        }
+    }
+
+}
