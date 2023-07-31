36d38e676139dc8f332d037499826e2a3ec60220
Guilherme Borret Oliveira Ferreira <gborret@icloud.com>
2023-07-30 23:20:24 -0300

    commit initial

---
+++ MeuApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var price = 10.2;
+        var texto = "O preço do produto é " + price + "apenas enquanto durar o estoque de promoção!";
+        Console.WriteLine(texto);
+    }
+}
